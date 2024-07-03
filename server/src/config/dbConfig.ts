96a787a7c6b5bb30c91f7f5c0acaecce8c540e27
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-03 09:27:29 +0530

    fix: fixed db config

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect(`${process.env.MONGO_URI}`)
+        await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log("Database connected")
     } catch (error) {
         console.log(error) 
