f851c2260b1febfb277068940a64c7e943842411
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-03 10:33:12 +0530

    feat : added data table

--- client/url-shortener/src/components/Containter.tsx
+++ client/url-shortener/src/components/Containter.tsx
@@ -3,12 +3,13 @@ import FormContainer from './FormContainer/FormContainer';
 import { UrlData } from '../interface/UrlData';
 import axios from 'axios';
 import { serverUrl } from './helpers/Constants';
+import DataTable from './FormContainer/DataTable';
 
-interface IAppProps {
+interface IContainerProps {
 }
 
-const Container: React.FunctionComponent<IAppProps> = () => {
-    const [data, setData] = React.useState<UrlData>();
+const Container: React.FunctionComponent<IContainerProps> = () => {
+    const [data, setData] = React.useState<UrlData[]>([]);
     const fetchTableData = async () => {
         const response = await axios.get(`${serverUrl}/shortUrl`)
         console.log("The response from the server is", response)
@@ -22,7 +23,8 @@ const Container: React.FunctionComponent<IAppProps> = () => {
 
     return (
         <div>
-            <FormContainer/>
+            <FormContainer />
+            <DataTable data={data} />
       </div>
   );
 };
--- client/url-shortener/src/interface/UrlData.ts
+++ client/url-shortener/src/interface/UrlData.ts
@@ -1,6 +1,7 @@
 export interface UrlData{
     _id: string;
     fullUrl: string;
+    shortUrl: string;
     clicks: number;
     createdAt: Date;
     updatedAt: Date;
